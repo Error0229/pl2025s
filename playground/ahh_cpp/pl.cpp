2b8cd807ef5c22884bf5e90af8908f7ac4e9c19e
gyra0229 <loginerrore04@gmail.com>
2025-04-30 22:32:03 +0800

    template and currying

--- playground/ahh_cpp/pl.cpp
+++ playground/ahh_cpp/pl.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 using namespace std;
-int Add(int a, int b)
-{
-    return a + b;
-}
-float Add(float a, float b)
-{
-    return a + b;
-}
 
-template <typename T, typename F>
-T GenericAdd(T a, F b)
+template <typename T>
+int bar(T f)
 {
-    return a + b;
+    int x = 5;
+    return f(x);
 }
 
 int main()
 {
-    int a, c;
-    float b;
-    cout << GenericAdd(a, b) << '\n';
+    int x = 20;
+    cout << bar([&x](int y) { return x++ + y; }) << endl;
+    cout << x << endl;
 }
--- playground/go_lambda/main.go
+++ playground/go_lambda/main.go
@@ -9,10 +9,15 @@ func Curry(f func(string) string) func(string) string {
 		return f(s)
 	}
 }
+func CurryAddXYZ(x int) func(int) func(int) int {
+	return func(y int) func(int) int {
+		return func(z int) int {
+			return x + y + z
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, " + Curry(func(s string) string {
-		return s + "!"
-	})("World"))
+	fmt.Println(CurryAddXYZ(1)(2)(3)) // 6
 
 }
